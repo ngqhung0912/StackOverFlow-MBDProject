b9fb8991b7300a10b6f93490fe23edaafe09e7d8
Colyn Jonker <colynjonker@hotmail.com>
2023-01-17 15:21:35 +0100

    Features added -> char, sentence count

--- codes/features.py
+++ codes/features.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Last modified on 16th Jan.
 Author: Hung.
@@ -5,15 +6,19 @@ Author: Hung.
 time spark-submit --master yarn --deploy-mode cluster  create_parquet.py
 
 """
-
+import re
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import sent_tokenize
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
 import sys
 from pyspark.sql.types import *
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as f
-import re
+
 from pyspark.sql.functions import udf, col
+
 
 
 def word_count(content):
@@ -24,27 +29,35 @@ def word_count(content):
 
     return text_length
 
-# SUbjectivity
-# Sentiment
-# Colemann
+
+def char_count(content):
+    return len(content)
+
+
+def sentence_count(content):
+    return len(sent_tokenize(content))
+
+
+# Coleman–Liau_index
+def coleman_liau(cleaned_text):
+    no_chars = char_count(cleaned_text)
+    no_sentences = sentence_count(cleaned_text)
+    no_words = word_count(cleaned_text)
+    L = no_chars / no_words
+    S = no_words / no_sentences
+    return 0.0588 * L - 0.296 * S - 15.8
+
 
 sc = SparkContext(appName="stack_exchange")
 sc.setLogLevel("ERROR")
 sqlContext = SQLContext(sc)
 spark = SparkSession.builder.getOrCreate()
 
-
-post_df = spark.read.parquet('/user/s2812940/project/parquet_data/posts.parquet/part-00000-dfdedfcd-0d15-452e-bab4-48f6cf9a8276-c000.snappy.parquet')
+post_df = spark.read.parquet(
+    '/user/s2812940/project/parquet_data/posts.parquet/part-00000-dfdedfcd-0d15-452e-bab4-48f6cf9a8276-c000.snappy.parquet')
 # Read a fraction of data (100 rows). Apply lambda function to count words.
 post_df_small = post_df.limit(1)
 
 word_count_udf = udf(lambda x: word_count(x))
 post_df_small = post_df_small.withColumn('bodyWordCount', word_count_udf(col('_Body')))
 print(post_df_small.show())
-
-
-
-
-
-
-
