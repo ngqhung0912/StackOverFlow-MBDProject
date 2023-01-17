7ed2ecc49f35b245d6a5a9073f76b55e9c6138c2
Hung Nguyen <ngqhung0912@gmail.com>
2023-01-17 14:43:27 +0100

    features.py added

---
+++ codes/features.py
@@ -0,0 +1,47 @@
+"""
+Last modified on 16th Jan.
+Author: Hung.
+
+time spark-submit --master yarn --deploy-mode cluster  create_parquet.py
+
+"""
+
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+import sys
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+import re
+from pyspark.sql.functions import udf, col
+
+
+def word_count(content):
+    rules = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+    cleantext = re.sub(rules, '', content.lower())
+    cleantext = re.sub('[^a-z0-9]+', ' ', cleantext)
+    text_length = len(cleantext.split())
+
+    return text_length
+
+
+
+sc = SparkContext(appName="stack_exchange")
+sc.setLogLevel("ERROR")
+sqlContext = SQLContext(sc)
+spark = SparkSession.builder.getOrCreate()
+
+
+post_df = spark.read.parquet('posts.parquet/part-00000-dfdedfcd-0d15-452e-bab4-48f6cf9a8276-c000.snappy.parquet')
+# Read a fraction of data (100 rows). Apply lambda function to count words.
+post_df_small = post_df.limit(1)
+
+word_count_udf = udf(lambda x: word_count(x))
+post_df_small = post_df_small.withColumn('bodyWordCount', word_count_udf(col('_Body')))
+
+
+
+
+
+
+
