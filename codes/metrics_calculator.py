e9da9f8a44a5ff11cba0d019f4f4c0973d822968
Hung Nguyen <ngqhung0912@gmail.com>
2023-02-06 13:56:26 +0100

    well-documented metrics-calculator file

--- codes/metrics_calculator.py
+++ codes/metrics_calculator.py
@@ -1,14 +1,13 @@
-'''
+"""
 LOCAL RUNNING:
 time spark-submit   --conf "spark.pyspark.python=./../../miniconda3/envs/bigdataEnv/bin/python"
 --conf "spark.pyspark.driver.python=../../miniconda3/envs/bigdataEnv/bin/python" metrics_calculator.py
 
 CLUSTER RUNNING:
- PYSPARK_PYTHON=./stackoverflow_env/stackoverflow_env/bin/python spark-submit --conf spark.dynamicAllocation.maxExecutors=15 --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=./stackoverflow_env/stackoverflow_env/bin/python --master yarn --deploy-mode cluster --archives stackoverflow_env.zip#stackoverflow_env  metrics_calculator.py
-'''
+
+PYSPARK_PYTHON=./stackoverflow_env/stackoverflow_env/bin/python spark-submit --conf spark.dynamicAllocation.maxExecutors=30 --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=./stackoverflow_env/stackoverflow_env/bin/python --master yarn --deploy-mode cluster --archives stackoverflow_env.zip#stackoverflow_env  metrics_calculator.py
+"""
 # from __future__ import division # THIS LINE IS FUCKING IMPORTANT FOR PYTHON 2 ONLY!!!!!
-
-
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql import SparkSession
@@ -24,12 +23,18 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 spark = SparkSession.builder.getOrCreate()
-nltk.data.path.append('/stackoverflow_env/stackoverflow_env/nltk_data')
+nltk.data.path.append('/stackoverflow_env/stackoverflow_env/nltk_data')  # Path for NLTK Data used: Vader package
 
-RULE_CODES = re.compile('<code>.*?</code>', re.DOTALL)
+RULE_CODES = re.compile('<code>.*?</code>', re.DOTALL)  # Regular expressions for extracting paragraph of code.
 
 
-def clean_data(text, codeless=True):
+def clean_data(text: str, codeless=True) -> str:
+    """
+    Remove HTML tags, code tags (if desired), etc. Make text lowercase.
+    :param text: Raw text paragraph.
+    :param codeless: Set to true will return cleaned text without HTML <code> blocks.
+    :return: Cleaned text.
+    """
     rules = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
     text = text.lower()
     if codeless:
@@ -38,19 +43,39 @@ def clean_data(text, codeless=True):
     return cleantext
 
 
-def word_count(content):
+def word_count(content: str) -> int:
+    """
+    Count the number of words in the content
+    :param content: Text content
+    :return: number of words in content.
+    """
     return len(content.split())
 
 
-def char_count(content):
+def char_count(content: str) -> int:
+    """
+    Count the number of characters in the content
+    :param content: Text content
+    :return: number of characters in content.
+    """
     return len(content)
 
 
-def sentence_count(content):
+def sentence_count(content: str) -> int:
+    """
+    Count the number of sentences in the content using NLTK's sent_tokenize.
+    :param content: Text content
+    :return: number of sentences in content.
+    """
     return len(sent_tokenize(content))
 
 
-def syllable_count(text):
+def syllable_count(text: str) -> int:
+    """
+    Count the number of syllable in the content.
+    :param text: Text content:
+    :return: Number of syllables.
+    """
     count = 0
     for word in text.split():
         vowels = "aeiouy"
@@ -68,32 +93,59 @@ def syllable_count(text):
     return count
 
 
-def coleman_liau(no_chars, no_sentences, no_words):
+def coleman_liau(no_chars: int, no_sentences: int, no_words: int) -> float:
+    """
+    Calculate the Coleman-Liau index for a paragraph.
+    :param no_chars: Number of characters.
+    :param no_sentences: Number of sentences.
+    :param no_words: Number of words.
+    :return: Coleman-Liau index.
+    """
     if no_words == 0:
-        return None
+        return -999
     L = (int(no_chars) / int(no_words)) * 100
     S = (int(no_sentences) / int(no_words)) * 100
     return 0.0588 * L - 0.296 * S - 15.8
 
 
-def flesch_ease(no_syllables, no_sentences, no_words):
+def flesch_ease(no_syllables: int, no_sentences: int, no_words: int) -> float:
+    """
+    Calculate the Flesch Reading Ease score for a paragraph.
+    :param no_syllables: Number of characters.
+    :param no_sentences: Number of sentences.
+    :param no_words: Number of words.
+    :return: Flesch Reading Ease score
+    """
     if no_sentences == 0 or no_words == 0:
-        return None
+        return -999
     words_per_sent = no_words / no_sentences
     syll_per_word = no_syllables / no_words
     return 206.835 - (1.015 * words_per_sent) - (84.6 * syll_per_word)
 
 
-def flesch_grade(no_syllables, no_sentences, no_words):
+def flesch_grade(no_syllables, no_sentences, no_words) -> float:
+    """
+    Calculate the Flesch-Kincaid score for a paragraph.
+    :param no_syllables: Number of characters.
+    :param no_sentences: Number of sentences.
+    :param no_words: Number of words.
+    :return: Flesch-Kincaid score
+    """
     if no_sentences == 0 or no_words == 0:
-        return None
+        return -999
     words_per_sent = no_words / no_sentences
     syll_per_word = no_syllables / no_words
     return 0.39 * words_per_sent + 11.8 * syll_per_word - 15.59
 
 
-def code_percentage(original_body, no_char):
-    code_text_list = re.findall(RULE_CODES, original_body)  # remove code block
+def code_percentage(original_body: str, no_char: int) -> float:
+    """
+    Calculate how many percent of post's character is inside the code block.
+    :param original_body: Original text (text that have not been cleaned).
+    :param no_char: Number of characters in the post.
+    :return: Percentage of code character.
+    """
+    code_text_list = re.findall(RULE_CODES, original_body)
     if code_text_list:
         code_texts = ''
         for code_text in code_text_list:
@@ -106,13 +158,24 @@ def code_percentage(original_body, no_char):
         return 0
 
 
-def calculate_cosine_similarity(body_1, body_2):
+def calculate_cosine_similarity(body_1: str, body_2: str) -> float:
+    """
+    Calculate the cosine similarity for two text bodies using scikit-learn's cosine_similarity and TfidfVectorizer.
+    :param body_1: Text body 1.
+    :param body_2: Text body 2.
+    :return: Cosine Similarity Scores.
+    """
     corpus = [body_1, body_2]
-    trsfm = TfidfVectorizer().fit_transform(corpus)
-    return cosine_similarity(trsfm[0], trsfm).tolist()[0][1]
+    transform_tfidf = TfidfVectorizer().fit_transform(corpus)
+    return cosine_similarity(transform_tfidf[0], transform_tfidf).tolist()[0][1]
 
 
-def clean_tags(raw_tags):
+def clean_tags(raw_tags: str) -> str:
+    """
+    Remove parentheses of tags list and append to a string.
+    :param raw_tags: Original tag lists with <> parentheses.
+    :return: A list of tags formatted as string, seperated by space.
+    """
     tag_lists = raw_tags.split('><')
     tag_str = ''
     for i in range(len(tag_lists)):
@@ -122,7 +185,13 @@ def clean_tags(raw_tags):
     return tag_str
 
 
-def clean_tags_list(raw_tags):
+def clean_tags_list(raw_tags: str) -> list:
+    """
+    Remove parentheses of tags list and append to a list.
+    :param raw_tags: Original tag lists with <> parentheses.
+    :return: A list of tags.
+    """
+
     tag_lists = raw_tags.split('><')
     tag_lists_cleaned = []
     for i in range(len(tag_lists)):
@@ -132,7 +201,15 @@ def clean_tags_list(raw_tags):
     return tag_lists_cleaned
 
 
-def calculate_metrics(original_body, original_title, original_tags):
+def calculate_metrics(original_body: str, original_title: str, original_tags: str) -> list:
+    """
+    Calculate the defined metrics: Flesch-Kincaid grade, Flesch Reading Ease, Coleman-Liau index, sentiment of post,
+    cosine similarity between post and title/tag and title.
+    :param original_body: Original (uncleaned body of post).
+    :param original_title: Original (uncleaned title of post).
+    :param original_tags: Original (uncleaned tags of post).
+    :return: A list of all metric scores.
+    """
     cleaned_text = clean_data(original_body)
     cleaned_title = clean_data(original_title)
     cleaned_tags = clean_tags(original_tags)
@@ -140,15 +217,17 @@ def calculate_metrics(original_body, original_title, original_tags):
     words = word_count(cleaned_text)
     characters = char_count(cleaned_text)
     sentences = sentence_count(cleaned_text)
-    flesch_kincaid_grade = flesch_grade(syllables, sentences, words)
-    flesch_reading_ease = flesch_ease(syllables, sentences, words)
-    coleman_liau_index = coleman_liau(characters, sentences, words)
-    code_percentages = code_percentage(original_body, characters)
-    sentiment = sia().polarity_scores(cleaned_text)['compound']
-    cosine_similarity_metrics_post_title = calculate_cosine_similarity(cleaned_title, cleaned_text)
-    cosine_similarity_metrics_tags_title = calculate_cosine_similarity(cleaned_tags, cleaned_title)
-    return [flesch_kincaid_grade, flesch_reading_ease, coleman_liau_index, code_percentages,
-            cosine_similarity_metrics_post_title, cosine_similarity_metrics_tags_title, sentiment]
+    if words > 10:
+        flesch_kincaid_grade = flesch_grade(syllables, sentences, words)
+        flesch_reading_ease = flesch_ease(syllables, sentences, words)
+        coleman_liau_index = coleman_liau(characters, sentences, words)
+        code_percentages = code_percentage(original_body, characters)
+        sentiment = sia().polarity_scores(cleaned_text)['compound']
+        cosine_similarity_metrics_post_title = calculate_cosine_similarity(cleaned_title, cleaned_text)
+        cosine_similarity_metrics_tags_title = calculate_cosine_similarity(cleaned_tags, cleaned_title)
+        return [flesch_kincaid_grade, flesch_reading_ease, coleman_liau_index, code_percentages,
+                cosine_similarity_metrics_post_title, cosine_similarity_metrics_tags_title, sentiment]
+    return [0] * 7
 
 
 data_path = ["posts.parquet/part-00000-dfdedfcd-0d15-452e-bab4-48f6cf9a8276-c000.snappy.parquet",
@@ -165,8 +244,7 @@ posts_df = spark.read.parquet(suuper_big_data_path).drop(*ignore_cols_posts)
 # questions = posts_df.na.drop(subset=["_AcceptedAnswerId"])
 questions = posts_df.filter(col('_PostTypeId') == 1)
 
-answers = posts_df.filter(col('_PostTypeId') == 2).select(col('_Id'), col('_Body'), col('_CreationDate')). \
-    withColumnRenamed('_Body', ''). \
+answers = posts_df.filter(col('_PostTypeId') == 2).select(col('_Id'), col('_CreationDate')). \
     withColumnRenamed('_CreationDate', 'AcceptedAnswerCreationDate'). \
     withColumnRenamed('_Id', 'AnswerId')
 
@@ -174,17 +252,19 @@ calculate_metrics_udf = \
     udf(lambda body, title, tags: calculate_metrics(body, title, tags), ArrayType(StringType()))
 
 questions = questions.withColumn("_CreationDate", to_timestamp("_CreationDate")). \
-    withColumn("Year", year("_CreationDate")).repartition(10, col('_AcceptedAnswerId'))
+    withColumn("Year", year("_CreationDate")).repartition(20, col('_AcceptedAnswerId'))
 
 years = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
-new_questions = questions.filter(col("Year") == 2008).sample(fraction=0.1)
+new_questions = questions.filter(col("Year") == 2008) \
+    .sample(fraction=0.1)
 for year in years:
-    questions_year = questions.filter(col("Year") == year).sample(fraction=0.1)
+    questions_year = questions.filter(col("Year") == year) \
+        .sample(fraction=0.1)
     new_questions = new_questions.union(questions_year)
 questions = new_questions
-answers = answers.repartition(10, col('AnswerId'))
+answers = answers.repartition(20, col('AnswerId'))
 
-questions = questions.join(answers, questions._AcceptedAnswerId == answers.AnswerId). \
+questions = questions.join(answers, questions._AcceptedAnswerId == answers.AnswerId, 'leftouter'). \
     withColumn("AcceptedAnswerCreationDate", to_timestamp("AcceptedAnswerCreationDate")). \
     withColumn('metrics',
                calculate_metrics_udf((col('_Body')), col('_Title'), col('_Tags'))). \
@@ -199,10 +279,10 @@ questions = questions.join(answers, questions._AcceptedAnswerId == answers.Answe
     withColumn('code_percentage', col('metrics')[3]). \
     withColumn('cos_sim_post_title', col('metrics')[4]). \
     withColumn('cos_sim_title_tag', col('metrics')[5]). \
-    withColumn('sentiment', col('metrics')[6]).\
+    withColumn('sentiment', col('metrics')[6]). \
     drop('_Body', 'Year')
 
-questions.repartition(10).write.mode('overwrite').\
-    parquet('questions-with-ans-and-metrics-cluster-FINAL-INCL-EVERYTHING.parquet')
+questions.repartition(20).write.mode('overwrite'). \
+    parquet('questions-with-ans-and-metrics-cluster-Final-BigDataALERT.parquet')
 
 # pyspark --conf "spark.pyspark.python=/usr/bin/python3.6"
